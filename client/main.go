60b4559fa32872c63add3fc10b7dd6b089b05332
icexin <fanbingxin.me@gmail.com>
2018-04-17 20:58:30 +0800

    ac

--- client/main.go
+++ client/main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-        "bufio"
-        "code.google.com/p/go.crypto/ssh/terminal"
-        "flag"
-        "fmt"
-        "github.com/kr/pty"
-        "io"
-        "log"
-        "net"
-        "os"
-        "os/exec"
-        "strings"
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/kr/pty"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 var addr = flag.String("addr", ":9981", "address of transporter server")
@@ -19,87 +20,87 @@ var shell = flag.String("sh", "/bin/bash", "shell")
 var id = flag.Int("id", -1, "connection id")
 
 func doConn(conn net.Conn, id int) {
-        defer conn.Close()
+	defer conn.Close()
 
-        fmt.Fprintf(conn, "CONN %d\r\n", id)
+	fmt.Fprintf(conn, "CONN %d\r\n", id)
 
-        r := bufio.NewReader(conn)
-        line, err := r.ReadString('\n')
-        if err != nil {
-                log.Print(err)
-                return
-        }
-        line = strings.Trim(line, "\r\n")
-        ret := strings.Fields(line)
-        if ret[0] != "200" {
-                log.Print(ret[1])
-                return
-        }
-        log.Print(ret)
-        go io.Copy(conn, os.Stdin)
-        io.Copy(os.Stdout, conn)
-        log.Print("Connection closed")
+	r := bufio.NewReader(conn)
+	line, err := r.ReadString('\n')
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	line = strings.Trim(line, "\r\n")
+	ret := strings.Fields(line)
+	if ret[0] != "200" {
+		log.Print(ret[1])
+		return
+	}
+	log.Print(ret)
+	go io.Copy(conn, os.Stdin)
+	io.Copy(os.Stdout, conn)
+	log.Print("Connection closed")
 }
 
 func doReg(conn net.Conn) {
-        defer conn.Close()
+	defer conn.Close()
 
-        fmt.Fprintf(conn, "REG\r\n")
+	fmt.Fprintf(conn, "REG\r\n")
 
-        r := bufio.NewReader(conn)
-        line, err := r.ReadString('\n')
-        if err != nil {
-                log.Print(err)
-                return
-        }
-        line = strings.Trim(line, "\r\n")
-        ret := strings.Fields(line)
-        if ret[0] != "200" {
-                log.Print(ret[1])
-                return
-        }
-        log.Printf("id: %s\n", ret[1])
+	r := bufio.NewReader(conn)
+	line, err := r.ReadString('\n')
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	line = strings.Trim(line, "\r\n")
+	ret := strings.Fields(line)
+	if ret[0] != "200" {
+		log.Print(ret[1])
+		return
+	}
+	log.Printf("id: %s\n", ret[1])
 
-        c := exec.Command(*shell)
-        fd, err := pty.Start(c)
+	c := exec.Command(*shell)
+	fd, err := pty.Start(c)
 
-        if err != nil {
-                log.Print(err)
-                return
-        }
-        var w io.Writer
-        w = io.MultiWriter(conn, os.Stdout)
-        go func() {
-                io.Copy(w, fd)
-                conn.Close()
-        }()
-        io.Copy(fd, conn)
-        // Write EOF
-        fd.Write([]byte{4})
-        c.Wait()
-        log.Print("Connection closed")
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	var w io.Writer
+	w = io.MultiWriter(conn, os.Stdout)
+	go func() {
+		io.Copy(w, fd)
+		conn.Close()
+	}()
+	io.Copy(fd, conn)
+	// Write EOF
+	fd.Write([]byte{4})
+	c.Wait()
+	log.Print("Connection closed")
 }
 
 func main() {
-        flag.Parse()
-        log.Print("Connecting ", *addr)
-        conn, err := net.Dial("tcp", *addr)
-        if err != nil {
-                log.Fatal(err)
-        }
+	flag.Parse()
+	log.Print("Connecting ", *addr)
+	conn, err := net.Dial("tcp", *addr)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-        log.Print("Connected")
+	log.Print("Connected")
 
-        oldState, err := terminal.MakeRaw(0)
-        if err != nil {
-                log.Fatal(err)
-        }
+	oldState, err := terminal.MakeRaw(0)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-        defer terminal.Restore(0, oldState)
+	defer terminal.Restore(0, oldState)
 
-        if *id != -1 {
-                doConn(conn, *id)
-        } else {
-                doReg(conn)
-        }
+	if *id != -1 {
+		doConn(conn, *id)
+	} else {
+		doReg(conn)
+	}
 }
